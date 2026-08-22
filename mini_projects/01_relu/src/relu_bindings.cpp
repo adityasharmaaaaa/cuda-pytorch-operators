d245995b5722cde8eb8c89aa6f291a4434af3302
Aditya Sharma <pvtadityaaa@gmail.com>
2026-08-22 11:43:30 +0530

    fixed pybind11

--- mini_projects/01_relu/src/relu_bindings.cpp
+++ mini_projects/01_relu/src/relu_bindings.cpp
@@ -1,13 +1,9 @@
 #include <torch/extension.h>
 
 torch::Tensor relu_forward_cuda(torch::Tensor input);
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME,m){
-    m.def("relu_forward",&relu_forward_cuda,"Custom ReLU forward (CUDA)");
-}
-
 torch::Tensor relu_backward_cuda(torch::Tensor grad_output, torch::Tensor output);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME,m){
+    m.def("relu_forward",&relu_forward_cuda,"Custom ReLU forward (CUDA)");
     m.def("relu_backward",&relu_backward_cuda, "Custom ReLU backward (CUDA)");
 }
